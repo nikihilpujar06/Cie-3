50d63337d534c3221de52ed1fcd39b2f01b4e633
nikihilpujar06 <nikihilpujar3@gmail.com>
2025-12-02 12:02:16 +0530

    Update test_vechile.py

--- test_vechile.py
+++ test_vechile.py
@@ -1,16 +1,16 @@
 def vehicle_details(v_num, o_name, v_type, year_of_mfg):
     return (
-        f"Vehicle Number : {v_num}\n"
+        f"Vehicle Number: {v_num}\n"
         f"Owner Name: {o_name}\n"
         f"Vehicle Type: {v_type}\n"
-        f"Year Of Manufacture : {year_of_mfg}"
+        f"Year of Manufacture: {year_of_mfg}"
     )
 
-if __name__ == "__main__":
-    v_num = "111"
-    o_name = "Jhon"
-    v_type = "Bike"
-    year_of_mfg = 2006
+expected = (
+    "Vehicle Number: 111\n"
+    "Owner Name: Jhon\n"
+    "Vehicle Type: Bike\n"
+    "Year of Manufacture: 2006"
+)
 
-    result = vehicle_details(v_num, o_name, v_type, year_of_mfg)
-    print(result)
+assert vehicle_details("111", "Jhon", "Bike", 2006) == expected
