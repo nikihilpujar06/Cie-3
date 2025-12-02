edd497a7453b3d78e6a9ae76fb54c0cedbd52897
nikihilpujar06 <nikihilpujar3@gmail.com>
2025-12-02 11:53:56 +0530

    Update vechile.py

--- vechile.py
+++ vechile.py
@@ -1,15 +1,15 @@
 def vechile_details(v_num,o_name,v_type,year_of_mfg):
     return (
-        f"Vechile Number : {v_num}"
-        f"Owner Name: {o_name}"
-        f"Vechile Type: {v_type}"
+        f"Vechile Number : {v_num}\n"
+        f"Owner Name: {o_name}\n"
+        f"Vechile Type: {v_type}\n"
         f"Year Of Manufacture : {year_of_mfg}"
     )
 
 if __name__ == "__main__":
-    v_num = "111"
-    o_name = "Jhon"
-    v_type = "Bike"
+    v_num = "111\n"
+    o_name = "Jhon\n"
+    v_type = "Bike\n"
     year_of_mfg = 2006
 
     result = vechile_details(v_num,o_name,v_type,year_of_mfg)
